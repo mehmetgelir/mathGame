be8ee0cd286f163fdcb771cc29b1b0957312b1b3
mehmetgelir <67960804+mehmetgelir@users.noreply.github.com>
2021-07-23 12:23:43 +0300

    s

---
+++ Assets/Scripts/GameLevel/SonucManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SonucManager : MonoBehaviour
+{
+    public void oyunaYenidenBasla()
+    {
+        SceneManager.LoadScene("gameLevel");
+    }
+
+    public void anaMenuyeDon()
+    {
+        SceneManager.LoadScene("menuLevel");
+    }
+}
---
+++ Assets/Scripts/GameLevel/alphaPanelManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class alphaPanelManager : MonoBehaviour
+{
+    public GameObject alphaPanel;
+    
+    void Start()
+    {
+    
+        alphaPanel.GetComponent<CanvasGroup>().DOFade(0, 2f);
+        
+    }
+
+
+    
+}
